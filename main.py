36ad15d25eaaaf1bdf2e2a7557934e7ff576daf4
April <april.y.luo@gmail.com>
2015-04-28 15:04:00 -0700

    latest front end changes, plus some sentiment stuff

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from datetime import datetime
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
+from nltk.model.ngram import NgramModel
+from nltk.probability import LidstoneProbDist
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+from textblob import TextBlob
 
 app = Flask(__name__)
 
@@ -20,12 +24,9 @@ class Conversation:
 #         self.timestamp = datetime.now()     # ?
 #         self.body = ''
 
-
-@app.route('/')
-def index():
-
-    with open('data/ankita.txt') as f:
-        ankita_conv = Conversation()
+def read_texts(textfile, sender):
+    with open(textfile) as f:
+        this_conv = Conversation()
         for line in f:
             line = line.strip()
             items = line.split('|')
@@ -34,23 +35,73 @@ def index():
                 tm["sender"] = items[0]
                 tm["timestamp"] = datetime.strptime(items[1], '%b %d, %Y, %I:%M %p')
                 tm["body"] = items[2].strip()
-                ankita_conv.outbound.append(tm)
-            elif items[0] == '+15103649591':
+                this_conv.outbound.append(tm)
+            elif items[0] == sender:
                 tm = {}
                 tm["sender"] = items[0]
                 tm["timestamp"] = datetime.strptime(items[1], '%b %d, %Y, %I:%M %p')
                 tm["body"] = items[2].strip()
-                ankita_conv.inbound.append(tm)
+                this_conv.inbound.append(tm)
             else:
                 pass
-
-    # sentiment_classifier = sentiment_train()
-    # print sentiment_classifier.classify(word_feats("Happy excellent magnificent outstanding"))
-
-    print(get_cosine_similarity(ankita_conv))
+    f.close()
+    return this_conv
 
 
+@app.route('/')
+def index():
     return render_template('index.html')
+
+
+@app.route('/ankita')
+def ankita():
+    ankita_conv = read_texts('data/ankita.txt', '+15103649591')
+    inbound_sentiments = []
+    outbound_sentiments = []
+
+    # sentiment analysis
+    inbound_sentiments, outbound_sentiments = get_sentiments(ankita_conv)
+
+    # trigram modeling
+    # lm = generate_language_model(ankita_conv.inbound)
+    # print lm
+
+    # cosine similarity
+    matrix = get_cosine_similarity(ankita_conv)[0]
+    sim = str(matrix).split(' ')[-1][:-1]
+
+    return render_template('friend.html',
+        name='Ankita Agharkar',
+        your_sentiments=outbound_sentiments,
+        friend_sentiments=inbound_sentiments,
+        cosine_sim=sim)
+
+
+@app.route('/riley')
+def riley():
+    riley_conv = read_texts('data/riley.txt', '+18186323954')
+    matrix = get_cosine_similarity(riley_conv)[0]
+    sim = str(matrix).split(' ')[-1][:-1]
+
+    return render_template('friend.html', name='Riley Pietsch', cosine_sim=sim)
+
+
+@app.route('/christina')
+def christina():
+    christina_conv = read_texts('data/christina.txt', '+19165217921')
+    matrix = get_cosine_similarity(christina_conv)[0]
+    sim = str(matrix).split(' ')[-1][:-1]
+
+    return render_template('friend.html', name='Christina Milanes', cosine_sim=sim)
+
+@app.route('/mom')
+def mom():
+    mom_conv = read_texts('data/mom.txt', '+14088211126')
+    matrix = get_cosine_similarity(mom_conv)[0]
+    sim = str(matrix).split(' ')[-1][:-1]
+
+    return render_template('friend.html', name='Helene Deng', cosine_sim=sim)
+
 
 def word_feats(words):
     return dict([(word, True) for word in words])
@@ -74,22 +125,49 @@ def sentiment_train():
     # classifier.show_most_informative_features()
     return classifier
 
+
+def get_sentiments(conv):
+    inbound = []
+    outbound = []
+
+    for tm in conv.inbound:
+        body = tm["body"].decode('ascii', errors='ignore')
+        score = TextBlob(body).sentiment[0]
+        inbound.append(score)
+
+    for tm in conv.outbound:
+        body = tm["body"].decode('ascii', errors='ignore')
+        score = TextBlob(body).sentiment[0]
+        outbound.append(score)
+
+    return (inbound, outbound)
+
+
+# Get the cosine similarity between two participants in a text conversation
 def get_cosine_similarity(conv):
     inbound = ""
     outbound = ""
     for tm in conv.inbound:
-        # inbound.append(tm["body"])
         inbound += tm["body"] + " "
     for tm in conv.outbound:
-        # outbound.append(tm["body"])
         outbound += tm["body"] + " "
-
-    # print inbound
-    # print outbound
 
     vect = TfidfVectorizer(min_df=1, decode_error='ignore')
     tfidf = vect.fit_transform([inbound, outbound])
     return (tfidf * tfidf.T).A
+
+
+# Given a list of a person's tokenized texts, generate a language model
+def generate_language_model(texts):
+    tokenized = []
+    for t in texts:
+        body = t["body"].split(" ")
+        for b in body:
+            tokenized.append(b)
+
+    est = lambda fdist : LidstoneProbDist(fdist, 0.2)
+    lm = NgramModel(3, tokenized, estimator=est)
+    return lm
 
 if __name__ == '__main__':
     app.debug = True
